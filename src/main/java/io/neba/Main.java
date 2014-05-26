f48b2c3eb79ec9912673456eabb416a18efb5414
Daniel Takai <daniel.takai@googlemail.com>
2014-05-26 14:17:56 +0200

    Added some source to verify signing, javadoc and source jar creations.

---
+++ src/main/java/io/neba/Main.java
@@ -0,0 +1,14 @@
+package io.neba;
+
+/**
+ * Main file for neba.
+ */
+public class Main {
+
+	/**
+	  * Method main for Main class of neba.
+	  */	
+	public void main(String arg[]) {
+		System.out.println("neba");
+	}
+}
