e2582fc878ef802cd6e355eea99d941d2ace165d
Olaf Otto <olaf.otto@unic.com>
2014-07-09 23:51:30 +0200

    Improved timestamp interpretation in the activity stream on neba.io

--- javascript/main.js
+++ javascript/main.js
@@ -390,6 +390,15 @@ $(function () {
     // Global for JSONP tweet callback
     window.loadTweets = window.loadTweets || [];
 
+    var PATTERN_GITHUB_TIMESTAMP = /([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]+)Z/;
+    var PATTERN_TWITTER_TIMESTAMP = /([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]+)\+[0-9]+/;
+
+    function parseTime(pattern, value) {
+        pattern.exec(value);
+        // Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
+        return new Date(RegExp.$1, RegExp.$2 - 1, RegExp.$3, RegExp.$4, RegExp.$5, RegExp.$6).getTime();
+    }
+
     function htmlEscape(str) {
         return String(str)
             .replace(/"/g, '&quot;')
@@ -429,7 +438,7 @@ $(function () {
                         type: "commit-" + owner + "-" + repo + "-" + branch,
                         message: '<a href="' + change.html_url + '">' + htmlEscape(change.commit.message) + '</a>',
                         url: change.html_url,
-                        time: Date.parse(change.commit.committer.date)
+                        time: parseTime(PATTERN_GITHUB_TIMESTAMP, change.commit.committer.date)
                     });
                 });
                 return result;
@@ -448,7 +457,7 @@ $(function () {
                         type: "issue",
                         message: '<a href="' + issue.url + '">' + issue.state + ' issue #' + issue.number + ": " + issue.title + '</a>',
                         url: issue.url,
-                        time: Date.parse(issue.updated_at)
+                        time: parseTime(PATTERN_GITHUB_TIMESTAMP, issue.updated_at)
                     });
                 });
                 return result;
@@ -461,7 +470,6 @@ $(function () {
     // this means we have to parse the widget HTML to retrieve data.
     var TweetsForWidget = function (widgetId) {
         var result = [];
-        var DATE_PATTERN = /([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]+)\+[0-9]+/;
 
         loadTweets.push(function (data) {
             $(data.body).find("li.tweet").each(function (_, elem) {
@@ -470,11 +478,7 @@ $(function () {
                 };
                 var $elem = $(elem);
                 $elem.find("time.dt-updated").each(function (_, elem) {
-                    // 2014-06-28T15:34:11+0000
-                    DATE_PATTERN.exec(elem.getAttribute("datetime"));
-                    // Date(year, month [, day [, hours[, minutes[, seconds[, ms]]]]])
-                    var date = new Date(RegExp.$1, RegExp.$2 - 1, RegExp.$3, RegExp.$4, RegExp.$5, RegExp.$6);
-                    tweet.time = date.getTime();
+                    tweet.time = parseTime(PATTERN_TWITTER_TIMESTAMP, elem.getAttribute("datetime"));
                 });
                 $elem.find("a.u-url").each(function (_, elem) {
                     tweet.url = elem.href;
