1f3ef17c261c46a14877acaf6d4b7ca28980a305
Olaf Otto <olaf.otto@unic.com>
2014-12-17 13:54:53 +0100

    http://neba.io site performance / mobile experience improvements: Merged CSS, merged JS, made JS asynchronous. Added missing prettyphoto elements.

--- javascript/prettyPhoto.js
+++
@@ -1,8 +0,0 @@
-/* ------------------------------------------------------------------------
-	Class: prettyPhoto
-	Use: Lightbox clone for jQuery
-	Author: Stephane Caron (http://www.no-margin-for-errors.com)
-	Version: 3.1.2
-------------------------------------------------------------------------- */
-
-
