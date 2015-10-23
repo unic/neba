442d3ee3cc014f7ef7f25105136b74c0dbafefc6
olaf-otto <olaf.otto@unic.com>
2015-10-23 10:59:31 +0200

    neba-89 the neba:defineObjects may yield the wrong model if the same synthetic resource is mapped...

--- core/src/main/java/io/neba/core/resourcemodels/adaptation/ResourceToModelAdapter.java
+++ core/src/main/java/io/neba/core/resourcemodels/adaptation/ResourceToModelAdapter.java
@@ -1,18 +1,18 @@
 /**
  * Copyright 2013 the original author or authors.
- * 
+ * <p/>
  * Licensed under the Apache License, Version 2.0 the "License";
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
-
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-**/
+ **/
 
 package io.neba.core.resourcemodels.adaptation;
 
@@ -34,55 +34,55 @@ import static org.apache.commons.lang.StringUtils.join;
 /**
  * Adapts a {@link Resource} to it's {@link io.neba.api.annotations.ResourceModel} using the
  * {@link ModelRegistry} to lookup and the {@link ResourceToModelMapper} to map the model.
- * 
+ *
  * @author Olaf Otto
  * @see ResourceToModelAdapterUpdater
  */
 @Service
 public class ResourceToModelAdapter implements AdapterFactory {
-	@Autowired
-	private ModelRegistry registry;
-	@Autowired
-	private ResourceToModelMapper mapper;
-	@Autowired
-	private ResourceModelCaches caches;
-	
-	/**
-	 * @return the resource model provided by the
+    @Autowired
+    private ModelRegistry registry;
+    @Autowired
+    private ResourceToModelMapper mapper;
+    @Autowired
+    private ResourceModelCaches caches;
+
+    /**
+     * @return the resource model provided by the
      * {@link io.neba.core.resourcemodels.registration.ModelRegistrar}
      *  or <code>null</code> if no model for the given resource exists or
      *  the resource model's type does not match the target type.
-	 */
+     */
     @Override
     public <T> T getAdapter(Object adaptable, Class<T> target) {
-		T model = null;
-		if (adaptable instanceof Resource) {
-			Resource resource = (Resource) adaptable;
-            Key key = new Key(resource.getPath(), target);
-			model = this.caches.lookup(key);
-			if (model == null) {
-				model = getAdapterInternal(target, resource);
+        T model = null;
+        if (adaptable instanceof Resource) {
+            Resource resource = (Resource) adaptable;
+            Key key = new Key(resource.getPath(), target, resource.getResourceType());
+            model = this.caches.lookup(key);
+            if (model == null) {
+                model = getAdapterInternal(target, resource);
                 if (model != null) {
                     this.caches.store(resource, model, key);
                 }
-			}
-		}
-		return model;
-	}
+            }
+        }
+        return model;
+    }
 
     @SuppressWarnings("unchecked")
-	private <T> T getAdapterInternal(Class<T> target, Resource resource) {
+    private <T> T getAdapterInternal(Class<T> target, Resource resource) {
         T model = null;
-		Collection<LookupResult> models = this.registry.lookupMostSpecificModels(resource, target);
-		if (models != null && !models.isEmpty()) {
-			if (models.size() == 1) {
+        Collection<LookupResult> models = this.registry.lookupMostSpecificModels(resource, target);
+        if (models != null && !models.isEmpty()) {
+            if (models.size() == 1) {
                 OsgiBeanSource<?> source = models.iterator().next().getSource();
                 model = (T) this.mapper.map(resource, source);
-			} else {
-				throw new AmbiguousModelAssociationException("There is more than one model that maps " +
-						resource.getPath() + " to " + target.getName() + ": " + join(models, ", ") + ".");
-			}
-		}
-		return model;
-	}
+            } else {
+                throw new AmbiguousModelAssociationException("There is more than one model that maps " +
+                        resource.getPath() + " to " + target.getName() + ": " + join(models, ", ") + ".");
+            }
+        }
+        return model;
+    }
 }
--- core/src/test/java/io/neba/core/resourcemodels/adaptation/ResourceToModelAdapterTest.java
+++ core/src/test/java/io/neba/core/resourcemodels/adaptation/ResourceToModelAdapterTest.java
@@ -1,180 +1,249 @@
 /**
  * Copyright 2013 the original author or authors.
- * 
+ * <p/>
  * Licensed under the Apache License, Version 2.0 the "License";
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
-
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-**/
+ **/
 
 package io.neba.core.resourcemodels.adaptation;
 
+import io.neba.core.resourcemodels.caching.ResourceModelCaches;
+import io.neba.core.resourcemodels.mapping.ResourceToModelMapper;
+import io.neba.core.resourcemodels.registration.LookupResult;
+import io.neba.core.resourcemodels.registration.ModelRegistry;
+import io.neba.core.util.Key;
+import io.neba.core.util.OsgiBeanSource;
+import org.apache.sling.api.resource.Resource;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import io.neba.core.resourcemodels.caching.ResourceModelCaches;
-import io.neba.core.resourcemodels.registration.LookupResult;
-import io.neba.core.resourcemodels.registration.ModelRegistry;
-import org.apache.sling.api.resource.Resource;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-
-import io.neba.core.resourcemodels.mapping.ResourceToModelMapper;
-import io.neba.core.util.OsgiBeanSource;
-
 /**
  * @author Olaf Otto
  */
 @RunWith(MockitoJUnitRunner.class)
 public class ResourceToModelAdapterTest {
-	
-	/**
-	 * @author Olaf Otto
-	 */
-	private static class TestModel {}
-	
-	/**
-	 * @author Olaf Otto
-	 */
-	private static class TestModelDerived extends TestModel {}
 
-	@Mock
-	private ModelRegistry registry;
-	@Mock
-	private ResourceToModelMapper mapper;
-	@Mock
-	private ResourceModelCaches caches;
+    /**
+     * @author Olaf Otto
+     */
+    private static class TestModel {
+    }
 
-	private Resource adaptable;
-	private Class<?> targetType;
-	private Object adapted;
-	private Object[] models;
-	private List<LookupResult> sources;
+    /**
+     * @author Olaf Otto
+     */
+    private static class TestModelDerived extends TestModel {
+    }
 
-	@InjectMocks
-	private ResourceToModelAdapter testee;
+    @Mock
+    private ModelRegistry registry;
+    @Mock
+    private ResourceToModelMapper mapper;
+    @Mock
+    private Resource resource;
+    @Mock
+    private ResourceModelCaches caches;
+    private Map<Key, Object> testCache = new HashMap<Key, Object>();
 
-	@Test
-	public void testAdaptationToExactModelType() throws Exception {
-		withAdaptable(mock(Resource.class));
-		withTargetType(TestModel.class);
-		withAvailableModels(new TestModel());
-		adapt();
-		verifyAdapterObtainsSourceFromRegistrar();
-		verifyAdapterMapsResoureToModel();
-		assertAdaptableWasAdaptedToModel();
-	}
+    private Class<?> targetType;
+    private Object adapted;
+    private Object[] models;
+    private List<LookupResult> sources;
 
-	@Test
-	public void testAdaptationToDerivedModelType() throws Exception {
-		withAdaptable(mock(Resource.class));
-		withTargetType(TestModel.class);
-		withAvailableModels(new TestModelDerived());
-		adapt();
-		verifyAdapterObtainsSourceFromRegistrar();
-		verifyAdapterMapsResoureToModel();
-		assertAdaptableWasAdaptedToModel();
-	}
+    @InjectMocks
+    private ResourceToModelAdapter testee;
 
-	@Test
-	public void testHandlingOfNullBeanSource() throws Exception {
-		withAdaptable(mock(Resource.class));
-		withTargetType(TestModel.class);
-		withNullReturnedAsBeanSourceFromRegistrar();
-		adapt();
-		verifyAdapterObtainsSourceFromRegistrar();
-		assertAdaptableWasNotAdaptedToModel();
-		verifyAdapterDoesNotMapResourceToModel();
-	}
-	
-	@Test(expected = AmbiguousModelAssociationException.class)
-	public void testAdaptationToTypeWithMultipleMappings() throws Exception {
-		withAdaptable(mock(Resource.class));
-		withTargetType(TestModel.class);
-		withAvailableModels(new TestModel(), new TestModelDerived());
-		adapt();
-	}
+    @Before
+    public void setUp() throws Exception {
+        Answer
+                storeInCache = new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                testCache.put((Key) invocation.getArguments()[2], invocation.getArguments()[1]);
+                return null;
+            }
+        },
+                lookupFromCache = new Answer() {
+                    @Override
+                    public Object answer(InvocationOnMock invocation) throws Throwable {
+                        Key key = (Key) invocation.getArguments()[0];
+                        return testCache.get(key);
+                    }
+                };
 
-	@SuppressWarnings("unchecked")
+        doAnswer(storeInCache).when(this.caches).store(isA(Resource.class), any(), isA(Key.class));
+        doAnswer(lookupFromCache).when(this.caches).lookup(isA(Key.class));
+    }
+
+    @Test
+    public void testAdaptationToExactModelType() throws Exception {
+        withTargetType(TestModel.class);
+        withAvailableModels(new TestModel());
+        adapt();
+        verifyAdapterObtainsSourceFromRegistrar();
+        verifyAdapterMapsResourceToModel();
+        assertResourceWasAdaptedToModel();
+    }
+
+    @Test
+    public void testAdaptationToDerivedModelType() throws Exception {
+        withTargetType(TestModel.class);
+        withAvailableModels(new TestModelDerived());
+        adapt();
+        verifyAdapterObtainsSourceFromRegistrar();
+        verifyAdapterMapsResourceToModel();
+        assertResourceWasAdaptedToModel();
+    }
+
+    @Test
+    public void testHandlingOfNullBeanSource() throws Exception {
+        withTargetType(TestModel.class);
+        withNullReturnedAsBeanSourceFromRegistrar();
+        adapt();
+        verifyAdapterObtainsSourceFromRegistrar();
+        assertResourceWasNotAdaptedToModel();
+        verifyAdapterDoesNotMapResourceToModel();
+    }
+
+    @Test(expected = AmbiguousModelAssociationException.class)
+    public void testAdaptationToTypeWithMultipleMappings() throws Exception {
+        withTargetType(TestModel.class);
+        withAvailableModels(new TestModel(), new TestModelDerived());
+        adapt();
+    }
+
+    @Test
+    public void testAdaptationOfSameResourceWithDifferentResourceTypeYieldsDifferentModel() throws Exception {
+        withResourceType("resource/type/one");
+        withResourcePath("/resource/path");
+        withTargetType(TestModel.class);
+        withAvailableModels(new TestModel());
+
+        adapt();
+        assertResourceWasAdaptedToModel();
+
+        withResourceType("resource/type/two");
+        withAvailableModels(new TestModelDerived());
+
+        adapt();
+        assertResourceWasAdaptedToModel();
+    }
+
+    @Test
+    public void testSubsequentAdaptationOfSameResourceWithSameResourceTypeIsServedFromCache() throws Exception {
+        withResourceType("resource/type/one");
+        withResourcePath("/resource/path");
+        withTargetType(TestModel.class);
+        withAvailableModels(new TestModel());
+
+        adapt();
+        verifyAdapterMapsResourceToModel();
+        assertResourceWasAdaptedToModel();
+
+        withResourceType("resource/type/one");
+
+        adapt();
+        verifyAdapterMapsResourceToModel();
+        assertResourceWasAdaptedToModel();
+    }
+
+    @SuppressWarnings("unchecked")
     private void verifyAdapterDoesNotMapResourceToModel() {
-		verify(this.mapper, never()).map(isA(Resource.class), isA(OsgiBeanSource.class));
-	}
+        verify(this.mapper, never()).map(isA(Resource.class), isA(OsgiBeanSource.class));
+    }
 
-	private void withNullReturnedAsBeanSourceFromRegistrar() {
-		when(this.registry.lookupMostSpecificModels(eq(this.adaptable))).thenReturn(null);
-		when(this.registry.lookupMostSpecificModels(eq(this.adaptable), eq(this.targetType))).thenReturn(null);
-	}
-	
-	private void assertAdaptableWasNotAdaptedToModel() {
-		assertThat(this.adapted).isNull();
-	}
-	
-	private void assertAdaptableWasAdaptedToModel() {
-		assertAdaptableWasAdaptedToModel(0);
-	}
-	
-	private void assertAdaptableWasAdaptedToModel(int i) {
-		assertThat(this.adapted).isSameAs(this.models[i]);
-	}
+    private void withNullReturnedAsBeanSourceFromRegistrar() {
+        when(this.registry.lookupMostSpecificModels(eq(this.resource))).thenReturn(null);
+        when(this.registry.lookupMostSpecificModels(eq(this.resource), eq(this.targetType))).thenReturn(null);
+    }
 
-	private void verifyAdapterObtainsSourceFromRegistrar() {
-		verify(this.registry, times(1)).lookupMostSpecificModels(eq(this.adaptable), eq(this.targetType));
-	}
+    private void assertResourceWasNotAdaptedToModel() {
+        assertThat(this.adapted).isNull();
+    }
 
-	private void verifyAdapterMapsResoureToModel() {
-		verifyAdapterMapsResoureToModel(0);
-	}
-	
-	private void verifyAdapterMapsResoureToModel(int i) {
+    private void assertResourceWasAdaptedToModel() {
+        assertResourceWasAdaptedToModel(0);
+    }
+
+    private void assertResourceWasAdaptedToModel(int i) {
+        assertThat(this.adapted).isSameAs(this.models[i]);
+    }
+
+    private void verifyAdapterObtainsSourceFromRegistrar() {
+        verify(this.registry, times(1)).lookupMostSpecificModels(eq(this.resource), eq(this.targetType));
+    }
+
+    private void verifyAdapterMapsResourceToModel() {
+        verifyAdapterMapsResourceToModel(0);
+    }
+
+    private void verifyAdapterMapsResourceToModel(int i) {
         OsgiBeanSource<?> source = this.sources.get(i).getSource();
-        verify(this.mapper).map(eq(this.adaptable), eq(source));
-	}
+        verify(this.mapper).map(eq(this.resource), eq(source));
+    }
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private void withAvailableModels(Object... models) {
-		this.models = models;
-		this.sources = new ArrayList<LookupResult>();
-		when(this.registry.lookupMostSpecificModels(isA(Resource.class), isA(Class.class))).thenReturn(this.sources);
-		
-		for (Object model: models) {
-			OsgiBeanSource source = mock(OsgiBeanSource.class);
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    private void withAvailableModels(Object... models) {
+        this.models = models;
+        this.sources = new ArrayList<LookupResult>();
+        when(this.registry.lookupMostSpecificModels(isA(Resource.class), isA(Class.class))).thenReturn(this.sources);
+
+        for (Object model : models) {
+            OsgiBeanSource source = mock(OsgiBeanSource.class);
             LookupResult result = mock(LookupResult.class);
             when(result.getSource()).thenReturn(source);
-			this.sources.add(result);
-			when(source.getBeanType()).thenReturn((Class) model.getClass());
-			when(source.getBean()).thenReturn(model);
-			when(this.mapper.map(eq(this.adaptable), eq(source))).thenReturn(model);
-		}
-	}
+            this.sources.add(result);
+            when(source.getBeanType()).thenReturn((Class) model.getClass());
+            when(source.getBean()).thenReturn(model);
+            when(this.mapper.map(eq(this.resource), eq(source))).thenReturn(model);
+        }
+    }
 
-	private void adapt() {
-		this.adapted = this.testee.getAdapter(this.adaptable, this.targetType);
-	}
+    private void adapt() {
+        this.adapted = this.testee.getAdapter(this.resource, this.targetType);
+    }
 
-	private void withTargetType(Class<TestModel> targetType) {
-		this.targetType = targetType;
-	}
+    private void withTargetType(Class<TestModel> targetType) {
+        this.targetType = targetType;
+    }
 
-	private void withAdaptable(Resource mock) {
-		this.adaptable = mock;
-	}
+    private void withResourcePath(String path) {
+        doReturn(path).when(resource).getPath();
+    }
+
+    private void withResourceType(String type) {
+        doReturn(type).when(this.resource).getResourceType();
+    }
 }
