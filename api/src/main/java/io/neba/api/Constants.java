86945b3585b71af8d99063118ee4117a57820e06
Olaf Otto <olaf.otto@unic.com>
2016-01-21 18:50:18 +0100

    neba-86 Remove velocity support

--- api/src/main/java/io/neba/api/Constants.java
+++ api/src/main/java/io/neba/api/Constants.java
@@ -30,26 +30,6 @@ public class Constants {
     public static final String MODEL = "m";
 
     /**
-     * The key under which the currently used
-     * {@link io.neba.api.rendering.BeanRenderer} is provided when
-     * rendering an object with a
-     * {@link io.neba.api.rendering.BeanRenderer}.
-     */
-    public static final String RENDERER = "renderer";
-
-    /**
-     * The name of the default
-     * {@link io.neba.api.rendering.BeanRenderer}. A renderer with
-     * this name should be configured in the NEBA bean renderer
-     * config in the sling console. The default renderer is used by the MVC
-     * framework and the render tag if no
-     * specific renderer name is defined.
-     * 
-     * @see io.neba.api.rendering.BeanRendererFactory
-     */
-    public static final String DEFAULT_RENDERER_NAME = "default";
-
-    /**
      * This virtual resource type is considered the root of the type hierarchy
      * of all synthetic resource types. This supports mapping a resource model
      * to all resources <em>including</em> synthetic resources.
