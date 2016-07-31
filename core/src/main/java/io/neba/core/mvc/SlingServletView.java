355fc6d9383f602522bbc14c7a93a1f6c9f6fccd
Olaf Otto <olaf@x100.de>
2016-08-01 01:27:49 +0200

    neba-144 MVC: Enable view resolution to Sling Scripting
    - Added script view resolution leveraging sling resource types
    - Enabled WebApplicationContext support and thereby spring form taglib support in the process
    - Enable ServletContextAware and ServletConfigAware in the process, thus improving compliance to standard WebApplicationContext setups

---
+++ core/src/main/java/io/neba/core/mvc/SlingServletView.java
@@ -0,0 +1,92 @@
+/*
+  Copyright 2013 the original author or authors.
+
+  Licensed under the Apache License, Version 2.0 the "License";
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+package io.neba.core.mvc;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.SyntheticResource;
+import org.apache.sling.api.wrappers.SlingHttpServletRequestWrapper;
+import org.springframework.web.servlet.View;
+
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Resolves a resource path to a {@link Servlet} representing a {@link org.apache.sling.api.scripting.SlingScript}
+ * and invokes the script to {@link #render(Map, HttpServletRequest, HttpServletResponse) render} the view.
+ *
+ * @author Olaf Otto
+ */
+public class SlingServletView implements View {
+    private final String resourceType;
+    private final Servlet servlet;
+
+    public SlingServletView(String resourceType, Servlet servlet) {
+        this.resourceType = resourceType;
+        this.servlet = servlet;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    /**
+     * @param model    can be <code>null</code>.
+     * @param request  must not be <code>null</code>.
+     * @param response must not be <code>null</code>.
+     */
+    @Override
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        final SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) request;
+        final ResourceResolver resourceResolver = slingRequest.getResourceResolver();
+        final String resourcePath = request.getPathInfo();
+
+        final Resource resource = new SyntheticResource(resourceResolver, resourcePath, this.resourceType);
+        final SlingHttpServletRequest wrapped = new MvcResourceRequest(slingRequest, resource);
+
+        if (model != null) {
+            for (Map.Entry<String, ?> entry : model.entrySet()) {
+                wrapped.setAttribute(entry.getKey(), entry.getValue());
+            }
+        }
+
+        servlet.service(wrapped, response);
+    }
+
+    /**
+     * Wraps the original controller request to provide a {@link java.util.ResourceBundle.Control} as the
+     * {@link #getResource() request's resource}.
+     *
+     * @author Olaf Otto
+     */
+    static class MvcResourceRequest extends SlingHttpServletRequestWrapper {
+        private final Resource resource;
+
+        MvcResourceRequest(SlingHttpServletRequest slingRequest, Resource resource) {
+            super(slingRequest);
+            this.resource = resource;
+        }
+
+        @Override
+        public Resource getResource() {
+            return resource;
+        }
+    }
+}
